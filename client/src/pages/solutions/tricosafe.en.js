eea8bac620a78481cd7efc9075c3ec8c5e9f4594
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-10 15:55:37 +0200

    add tricosafe page
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- client/src/pages/solutions/tricosafe.en.js
+++ client/src/pages/solutions/tricosafe.en.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../../components/layout.en"
 import SEO from "../../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
+import background1 from "../../images/Export/templat_attracnt.png"
+import Img from "gatsby-image"
 
 import "../../components/solution.en.scss"
 
@@ -10,12 +12,64 @@ const TricoSafePage = () => {
     query TricosafeData {
       allTricoSafeJson {
         nodes {
-          title
           section1 {
+            title
+          }
+          section5 {
+            paragraphs {
+              id
+              contents {
+                title
+                content
+              }
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            title
+          }
+          section4 {
             title
             paragraphs {
               contents {
+                content
                 title
+              }
+              id
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+          section3 {
+            title
+            paragraphs {
+              id
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              contents {
+                content
+                title
+              }
+            }
+          }
+          section2 {
+            paragraphs {
+              id
+              contents {
                 content
               }
             }
@@ -28,23 +82,62 @@ const TricoSafePage = () => {
   return (
     <Layout>
       <SEO title="Gronic Attractants" />
-      <section className="gronic-solution-section">
-        <h1>{pageData.section1.title}</h1>
-        <Paragraphs data={pageData.section1.paragraphs} />
-      </section>
+      <div className="solution-page">
+        <section
+          className="gronic-solution-section solution-section-1"
+          style={{ backgroundImage: `url(${background1})` }}
+        >
+          <h1>{pageData.section1.title}</h1>
+        </section>
+        <section className="gronic-solution-section solution-section-2">
+          <Paragraphs data={pageData.section2.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-3">
+          <h1>{pageData.section3.title}</h1>
+          <div className="gronic-underline" />
+          <div className="paragraphs-container">
+            <Paragraphs data={pageData.section3.paragraphs} />
+          </div>
+        </section>
+        <section className="gronic-solution-section solution-section-3">
+          <h1>{pageData.section4.title}</h1>
+          <div className="gronic-underline" />
+          <div className="paragraphs-container">
+            <Paragraphs data={pageData.section4.paragraphs} />
+          </div>
+        </section>
+        <section className="gronic-solution-section solution-section-4">
+          <h1>{pageData.section5.title} </h1>
+          <div className="gronic-underline" />
+          <p className="section-subtitle"> 
+              {pageData.section5.paragraphs[0].contents.content}
+          </p>
+            <Paragraphs data={pageData.section5.paragraphs.slice(1)} />
+        </section>
+      </div>
     </Layout>
   )
 }
+
 
 const Paragraphs = ({ data }) => {
   return data.map(paragraph => {
     return (
       <div id={paragraph.id} className="gronic-solution-paragraph">
-        <h2> {paragraph.contents.title} </h2>
-        <p>{paragraph.contents.content}</p>
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.content}</p>
+        </div>
       </div>
     )
   })
 }
-
 export default TricoSafePage
