284b242aa066c77ae3e8b64b3088dae9d7ac2b4c
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-10 20:30:19 +0200

    fix logo
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -28,11 +28,11 @@ module.exports = {
       options: {
         name: `Gronic-Agricultrual-Firm-Website`,
         short_name: `gronic`,
-        start_url: `/`,
+        start_url: `/en`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
