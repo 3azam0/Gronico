f678977c6bb267a4641673547d6b2b9a42993f81
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-10 18:36:13 +0200

    add prosafe image
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

---
+++ client/src/pages/solutions/prosafe.en.js
@@ -0,0 +1,118 @@
+import React from "react"
+import Layout from "../../components/layout.en"
+import SEO from "../../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import background1 from "../../images/Export/templat_attracnt.png"
+import Img from "gatsby-image"
+
+import "../../components/solution.en.scss"
+
+const ProSafePage = () => {
+  const data = useStaticQuery(graphql`
+    query ProSafeData {
+      allProsafeJson {
+        nodes {
+          section1 {
+            title
+            backgroundImgURL
+          }
+          section2 {
+            paragraphs {
+              id
+              contents {
+                content
+                title
+              }
+            }
+          }
+          section3 {
+            paragraphs {
+              id
+              contents {
+                title
+                content {
+                  appearance
+                  lifeCycle
+                  pests
+                }
+              }
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const pageData = data.allProsafeJson.nodes[0]
+  return (
+    <Layout>
+      <SEO title="Gronic Attractants" />
+      <div className="solution-page">
+        <section
+          className="gronic-solution-section solution-section-1"
+          style={{ backgroundImage: `url(${background1})` }}
+        >
+          <h1>{pageData.section1.title}</h1>
+        </section>
+        <section className="gronic-solution-section solution-section-2">
+          <Paragraphs data={pageData.section2.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-3">
+          <ProsafeParagraphs data={pageData.section3.paragraphs} />
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+const Paragraphs = ({ data, hideImage }) => {
+  return data.map(paragraph => {
+    return (
+      <div id={paragraph.id} className="gronic-solution-paragraph">
+        {paragraph.backgroundImgURL && !hideImage ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.content}</p>
+        </div>
+      </div>
+    )
+  })
+}
+
+const ProsafeParagraphs = ({ data, hideImage }) => {
+  return data.map(paragraph => {
+    return (
+      <div id={paragraph.id} className="prosafe-paragraph">
+        <div className="content-container" style={{textAlign:'left',padding:'5%'}}>
+          <h2> {paragraph.contents.title} </h2>
+          <p><b>Appearance : </b>{paragraph.contents.content.appearance}</p>
+          <p><b>Life Cycle : </b>{paragraph.contents.content.lifeCycle}</p>
+          <p><b>Insect Pests Attacked :</b>{paragraph.contents.content.lifeCycle}</p>
+        </div>
+{paragraph.backgroundImgURL && !hideImage ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+      </div>
+    )
+  })
+}
+
+export default ProSafePage
