600f73f3f6b32e44bb081908016c9a64b9dfcd88
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-10 12:08:16 +0200

    create solution template
            * attractants
            * lures
    
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- client/src/pages/solutions/attractants.en.js
+++ client/src/pages/solutions/attractants.en.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../../components/layout.en"
 import SEO from "../../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
+import background1 from "../../images/Export/templat_attracnt.png"
+import Img from "gatsby-image"
 
 import "../../components/solution.en.scss"
 
@@ -14,10 +16,16 @@ const AttractantsPage = () => {
           section5 {
             paragraphs {
               id
-              imageURL
               contents {
-                contents
                 title
+                contents
+              }
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
               }
             }
             title
@@ -30,14 +38,26 @@ const AttractantsPage = () => {
                 title
               }
               id
-              imageURL
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
           }
           section3 {
             title
             paragraphs {
-              backgroundImgURL
               id
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
               contents {
                 content
                 title
@@ -50,7 +70,6 @@ const AttractantsPage = () => {
               contents {
                 content
               }
- 
             }
           }
           section1 {
@@ -64,35 +83,85 @@ const AttractantsPage = () => {
   const pageData = data.allAttractantsJson.nodes[0]
   return (
     <Layout>
-        <SEO title="Gronic Attractants"/>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section1.title}</h1>
-      </section>
-      <section className="gronic-solution-section">
-          <Paragraphs data={pageData.section2.paragraphs}/>
-      </section>
-      <section className="gronic-solution-section"> 
+      <SEO title="Gronic Attractants" />
+      <div className="solution-page">
+        <section
+          className="gronic-solution-section solution-section-1"
+          style={{ backgroundImage: `url(${background1})` }}
+        >
+          <h1>{pageData.section1.title}</h1>
+        </section>
+        <section className="gronic-solution-section solution-section-2">
+          <Paragraphs data={pageData.section2.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-3">
           <h1>{pageData.section3.title}</h1>
-          <Paragraphs data={pageData.section3.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
+          <div className="gronic-underline" />
+          <div className="paragraphs-container">
+            <Paragraphs data={pageData.section3.paragraphs} />
+          </div>
+        </section>
+        <section className="gronic-solution-section solution-section-4">
           <h1>{pageData.section4.title} </h1>
-          <Paragraphs data={pageData.section4.paragraphs}/>
-      </section>
+          <div className="gronic-underline" />
+          <Paragraphs data={pageData.section4.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-5">
+          <h1>{pageData.section5.title} </h1>
+          <div className="gronic-underline" />
+          <p className="section-subtitle">
+            {pageData.section5.paragraphs[0].contents.content}{" "}
+          </p>
+          <div className="gronic-solution-paragraphs">
+            <OurProducts data={pageData.section5.paragraphs.slice(1)} />
+          </div>
+        </section>
+      </div>
     </Layout>
   )
 }
 
-const Paragraphs = (({data}) => {
-    return data.map((paragraph) => {
-        return (
-            <div id={paragraph.id} className="gronic-solution-paragraph">
-                <h2> {paragraph.contents.title} </h2>
-                <p>{paragraph.contents.content}</p>
-            </div>
-        )
+const Paragraphs = ({ data }) => {
+  return data.map(paragraph => {
+    return (
+      <div id={paragraph.id} className="gronic-solution-paragraph">
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.content}</p>
+        </div>
+      </div>
+    )
+  })
+}
 
-    })
-})
-
-export default AttractantsPage;
+const OurProducts = ({ data }) => {
+  return data.map(paragraph => {
+    return (
+      <div id={paragraph.id} className="gronic-solution-paragraph">
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.contents}<br/>
+          <a className="product-contact-us" href="/en/contact"> Contanct Us </a>
+      </p>
+        </div>
+      </div>
+    )
+  })
+}
+export default AttractantsPage
--- client/src/pages/solutions/lures.en.js
+++ client/src/pages/solutions/lures.en.js
@@ -2,6 +2,9 @@ import React from "react"
 import Layout from "../../components/layout.en"
 import SEO from "../../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import background1 from "../../images/Export/templat_attracnt.png"
 
 import "../../components/solution.en.scss"
 
@@ -26,10 +29,16 @@ const LuresPage = () => {
           section3 {
             paragraphs {
               contents {
-                backgroundImgURL
                 content
               }
               id
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
             title
           }
@@ -41,7 +50,13 @@ const LuresPage = () => {
                 title
               }
               id
-              imageURL
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
           }
         }
@@ -52,20 +67,29 @@ const LuresPage = () => {
   return (
     <Layout>
       <SEO title="Gronic Attractants" />
-      <section className="gronic-solution-section">
-        <h1>{pageData.section1.title}</h1>
-      </section>
-      <section className="gronic-solution-section">
-        <Paragraphs data={pageData.section2.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section3.title}</h1>
-        <Paragraphs data={pageData.section3.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section4.title} </h1>
-        <Paragraphs data={pageData.section4.paragraphs} />
-      </section>
+      <div className="solution-page">
+        <section
+          className="gronic-solution-section solution-section-1"
+          style={{ backgroundImage: `url(${background1})` }}
+        >
+          <h1>{pageData.section1.title}</h1>
+        </section>
+        <section className="gronic-solution-section solution-section-2">
+          <Paragraphs data={pageData.section2.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-3">
+          <h1>{pageData.section3.title}</h1>
+          <div className="gronic-underline" />
+          <div className="paragraphs-container">
+            <Paragraphs data={pageData.section3.paragraphs} />
+          </div>
+        </section>
+        <section className="gronic-solution-section solution-section-4">
+          <h1>{pageData.section4.title} </h1>
+          <div className="gronic-underline" />
+          <Paragraphs data={pageData.section4.paragraphs} />
+        </section>
+      </div>
     </Layout>
   )
 }
@@ -74,8 +98,18 @@ const Paragraphs = ({ data }) => {
   return data.map(paragraph => {
     return (
       <div id={paragraph.id} className="gronic-solution-paragraph">
-        <h2> {paragraph.contents.title} </h2>
-        <p>{paragraph.contents.content}</p>
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.content}</p>
+        </div>
       </div>
     )
   })
