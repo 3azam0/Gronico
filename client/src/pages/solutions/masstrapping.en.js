9f88f3f23bf0be396a5a1cc257b4e114efb38413
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-10 14:56:52 +0200

    add masstrapping page
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- client/src/pages/solutions/masstrapping.en.js
+++ client/src/pages/solutions/masstrapping.en.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../../components/layout.en"
 import SEO from "../../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
+import background1 from "../../images/Export/templat_attracnt.png"
+import Img from "gatsby-image"
 
 import "../../components/solution.en.scss"
 
@@ -28,6 +30,13 @@ const MassTrappingPage = () => {
                 title
               }
               id
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
             title
           }
@@ -38,7 +47,13 @@ const MassTrappingPage = () => {
                 title
               }
               id
-              imgURL
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
             title
           }
@@ -48,7 +63,13 @@ const MassTrappingPage = () => {
                 content
                 title
               }
-              imgURL
+              backgroundImgURL {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
               id
             }
             title
@@ -60,23 +81,37 @@ const MassTrappingPage = () => {
   const pageData = data.allMassTrappingJson.nodes[0]
   return (
     <Layout>
-      <SEO title="Gronic Attractants" />
-      <section className="gronic-solution-section">
-        <h1>{pageData.section1.title}</h1>
-        <Paragraphs data={pageData.section1.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section2.title}</h1>
-        <Paragraphs data={pageData.section2.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section3.title}</h1>
-        <Paragraphs data={pageData.section3.paragraphs} />
-      </section>
-      <section className="gronic-solution-section">
-        <h1>{pageData.section4.title} </h1>
-        <Paragraphs data={pageData.section4.paragraphs} />
-      </section>
+      <SEO title="Gronic Mass Trapping" />
+      <div className="solution-page">
+        <section
+          className="gronic-solution-section solution-section-1"
+          style={{ backgroundImage: `url(${background1})` }}
+        >
+          <h1>{pageData.section1.title}</h1>
+        </section>
+        <section className="gronic-solution-section solution-section-2">
+          <Paragraphs data={pageData.section1.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-3">
+          <h1>{pageData.section2.title}</h1>
+          <div className="gronic-underline" />
+          <div className="paragraphs-container">
+            <Paragraphs data={pageData.section2.paragraphs} />
+          </div>
+        </section>
+        <section className="gronic-solution-section solution-section-4">
+          <h1>{pageData.section3.title}</h1>
+          <div className="gronic-underline" />
+          <Paragraphs data={pageData.section3.paragraphs} />
+        </section>
+        <section className="gronic-solution-section solution-section-5">
+          <h1>{pageData.section4.title} </h1>
+          <div className="gronic-underline" />
+          <div className="gronic-solution-paragraphs">
+            <OurProducts data={pageData.section4.paragraphs} />
+          </div>
+        </section>
+      </div>
     </Layout>
   )
 }
@@ -85,11 +120,48 @@ const Paragraphs = ({ data }) => {
   return data.map(paragraph => {
     return (
       <div id={paragraph.id} className="gronic-solution-paragraph">
-        <h2> {paragraph.contents.title} </h2>
-        <p>{paragraph.contents.content}</p>
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>{paragraph.contents.content}</p>
+        </div>
       </div>
     )
   })
 }
 
+const OurProducts = ({ data }) => {
+  return data.map(paragraph => {
+    return (
+      <div id={paragraph.id} className="gronic-solution-paragraph">
+        {paragraph.backgroundImgURL ? (
+          <Img
+            alt={paragraph.contents.title}
+            className="section-image"
+            fluid={paragraph.backgroundImgURL.childImageSharp.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        ) : null}
+        <div className="content-container">
+          <h2> {paragraph.contents.title} </h2>
+          <p>
+            {paragraph.contents.content}
+            <br />
+            <a className="product-contact-us" href="/en/contact">
+              {" "}
+              Contanct Us{" "}
+            </a>
+          </p>
+        </div>
+      </div>
+    )
+  })
+}
 export default MassTrappingPage
