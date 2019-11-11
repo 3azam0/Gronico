36e10e97514a5ea1c3034b94a223b8c99f35ebc9
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-11-11 23:04:14 +0200

    use nodes instead of edges in nav queries
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- client/src/components/navbar/navbar.en.js
+++ client/src/components/navbar/navbar.en.js
@@ -1,164 +1,189 @@
 import React from "react"
-import {useState} from "react"
-import {Link} from "gatsby"
+import { useState } from "react"
+import { Link } from "gatsby"
 import Img from "gatsby-image"
-import {useStaticQuery, graphql} from "gatsby"
-import {FaAngleDown, FaAngleRight, FaTimes} from "react-icons/fa"
-import {MdMenu} from "react-icons/md"
+import { useStaticQuery, graphql } from "gatsby"
+import { FaAngleDown, FaAngleRight, FaTimes } from "react-icons/fa"
+import { MdMenu } from "react-icons/md"
 
 import "./navbar.en.scss"
 
-
-const SubNavLinks = ({subLinks,subListClassName,subLinkClassName, subListStyle, subLinkStyle}) => {
-    subListClassName = subListClassName? "gronic-navSubList " + subListClassName : "gronic-navSubList"
-    subLinkClassName= subLinkClassName? "gronic-navSubLink " + subLinkClassName : "gronic-navSubLink"
-    console.log(subLinks)
-    const subNavLinks= subLinks? subLinks.map((sublink) =>
-        <li
-            key={sublink.url}
-            className={subListClassName} 
-            style={subListStyle}
-        >
-            <Link
-                className={subLinkClassName} 
-                style={subLinkStyle} 
-                to={sublink.url}
-            >{sublink.text} {sublink.subLinks ? <FaAngleRight className={subLinkClassName}/> : null}</Link>
-            {sublink.subLinks ? <ul><SubNavLinks subLinks={sublink.subLinks}/></ul> : null}
-            <hr className="nav-separator"/>
-        </li>
-    ) : null
-    return subNavLinks
-}
-
-const MainNavLinks = ({links,listClassName,linkClassName, listStyle, linkStyle}) => {
-    linkClassName =  linkClassName? "gronic-navLink " + linkClassName : "gronic-navLink"
-    listClassName = listClassName? "gronic-navListItem " + listClassName : "gronic-navListItem";
-    const mainLinks = links.links.map((link)=>
-        <li
-            key={link.url} 
-            className={listClassName}
-            style={listStyle}
-        >
-            <Link 
-                className={linkClassName} 
-                style={linkStyle} 
-                to={link.url}
-            >
-                {link.text} {link.subLinks ? <FaAngleDown style={{fontSize:'1rem'}} className={linkClassName}/> : null}
-                <div className="underline"></div>
-            </Link>
-            <ul>
-                <SubNavLinks subLinks = {link.subLinks}/>
-            </ul>
-        </li>
-    )
-    return mainLinks
-}
-
-const SmallNavLinks = ({showSmallNav,links, listClassName, linkClassName, listStyle, linkStyle}) => {
-    linkClassName =  linkClassName? "gronic-smallNavLink " + linkClassName : "gronic-smallNavLink"
-    listClassName = listClassName? "gronic-smallNavListItem" + listClassName : "gronic-smallNavListItem";
-
-    let smallNavLinks = null;
-    if (showSmallNav) {
-        smallNavLinks = links.links.map((link)=>
-            <li
-                key={link.url} 
-                className={listClassName}
-                style={listStyle}
-            >
-                <Link 
-                    className={linkClassName} 
-                    style={linkStyle} 
-                    to={link.url}
-                >
-                    {link.text}
-                    <div className="underline"></div>
-                </Link>
-            </li>
-        )            
-    }
-
-    return smallNavLinks
-}
-
-const Navbar = ({
-    navigationClassName,
-    navigationStyle
+const SubNavLinks = ({
+  subLinks,
+  subListClassName,
+  subLinkClassName,
+  subListStyle,
+  subLinkStyle,
 }) => {
-    const data = useStaticQuery(graphql`
-        query EnglishNavLinks{
-            allNavbarJson {
-                edges {
-                    node {
-                        id
-                        english {
-                            links {
-                                text
-                                url
-                                subLinks {
-                                    text
-                                    url
-                                    subLinks {
-                                        text
-                                        url
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            logo: file(relativePath: { eq: "logo.png" }) {
-                childImageSharp {
-                    fluid(maxWidth:500,maxHeight:713) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-        }
-    `)
-    const navLinksData = data.allNavbarJson.edges[0].node.english;
-    const [showSmallNav, setSmallNav] = useState(false);
-    const toggleNav = () => {
-        document.body.style.overflow = "hidden";
-        setSmallNav(!showSmallNav)
-    }
-    return(
-        <>
-            <nav
-                role="navigation"
-                className={'gronic-nav '+ navigationClassName}
-                style={navigationStyle}
-            > 
-                <Link to="/en">
-                    <Img
-                        alt="Gronic"
-                        className="gronic-navLogo"
-                        fluid={data.logo.childImageSharp.fluid}
-                        objectFit="cover"
-                        imgStyle={{ objectFit:'conain'}}
-                        style={{padding:'5%',position:'absolute',height:'2rem',width:'3re'}}
-                    />
-                </Link>
-                <ul className="gronic-navUlist">
-                    <MainNavLinks links={navLinksData}/>
-                </ul>
-                <a onClick={toggleNav} className="gronic-burger"><MdMenu/> </a>
-            </nav>
-           { showSmallNav && 
-               <nav className="gronic-smallNav">
-                    <button onClick={toggleNav} className="gronic-burger"><FaTimes/> </button>
-                    <ul>
-                        <SmallNavLinks  showSmallNav = {showSmallNav} links={navLinksData}/>
-                    </ul>
-                </nav>
-                }
-      
-        </>
-    )
+  subListClassName = subListClassName
+    ? "gronic-navSubList " + subListClassName
+    : "gronic-navSubList"
+  subLinkClassName = subLinkClassName
+    ? "gronic-navSubLink " + subLinkClassName
+    : "gronic-navSubLink"
+  console.log(subLinks)
+  const subNavLinks = subLinks
+    ? subLinks.map(sublink => (
+        <li key={sublink.url} className={subListClassName} style={subListStyle}>
+          <Link
+            className={subLinkClassName}
+            style={subLinkStyle}
+            to={sublink.url}
+          >
+            {sublink.text}{" "}
+            {sublink.subLinks ? (
+              <FaAngleRight className={subLinkClassName} />
+            ) : null}
+          </Link>
+          {sublink.subLinks ? (
+            <ul>
+              <SubNavLinks subLinks={sublink.subLinks} />
+            </ul>
+          ) : null}
+          <hr className="nav-separator" />
+        </li>
+      ))
+    : null
+  return subNavLinks
 }
 
+const MainNavLinks = ({
+  links,
+  listClassName,
+  linkClassName,
+  listStyle,
+  linkStyle,
+}) => {
+  linkClassName = linkClassName
+    ? "gronic-navLink " + linkClassName
+    : "gronic-navLink"
+  listClassName = listClassName
+    ? "gronic-navListItem " + listClassName
+    : "gronic-navListItem"
+  const mainLinks = links.links.map(link => (
+    <li key={link.url} className={listClassName} style={listStyle}>
+      <Link className={linkClassName} style={linkStyle} to={link.url}>
+        {link.text}{" "}
+        {link.subLinks ? (
+          <FaAngleDown style={{ fontSize: "1rem" }} className={linkClassName} />
+        ) : null}
+        <div className="underline"></div>
+      </Link>
+      <ul>
+        <SubNavLinks subLinks={link.subLinks} />
+      </ul>
+    </li>
+  ))
+  return mainLinks
+}
+
+const SmallNavLinks = ({
+  showSmallNav,
+  links,
+  listClassName,
+  linkClassName,
+  listStyle,
+  linkStyle,
+}) => {
+  linkClassName = linkClassName
+    ? "gronic-smallNavLink " + linkClassName
+    : "gronic-smallNavLink"
+  listClassName = listClassName
+    ? "gronic-smallNavListItem" + listClassName
+    : "gronic-smallNavListItem"
+
+  let smallNavLinks = null
+  if (showSmallNav) {
+    smallNavLinks = links.links.map(link => (
+      <li key={link.url} className={listClassName} style={listStyle}>
+        <Link className={linkClassName} style={linkStyle} to={link.url}>
+          {link.text}
+          <div className="underline"></div>
+        </Link>
+      </li>
+    ))
+  }
+
+  return smallNavLinks
+}
+
+const Navbar = ({ navigationClassName, navigationStyle }) => {
+  const data = useStaticQuery(graphql`
+    query EnglishNavLinks {
+      allNavbarJson {
+        nodes {
+          english {
+            links {
+              text
+              url
+              subLinks {
+                text
+                url
+                subLinks {
+                  text
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+      logo: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 713) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  const navLinksData = data.allNavbarJson.nodes[0].node.english
+  const [showSmallNav, setSmallNav] = useState(false)
+  const toggleNav = () => {
+    document.body.style.overflow = "hidden"
+    setSmallNav(!showSmallNav)
+  }
+  return (
+    <>
+      <nav
+        role="navigation"
+        className={"gronic-nav " + navigationClassName}
+        style={navigationStyle}
+      >
+        <Link to="/en">
+          <Img
+            alt="Gronic"
+            className="gronic-navLogo"
+            fluid={data.logo.childImageSharp.fluid}
+            objectFit="cover"
+            imgStyle={{ objectFit: "conain" }}
+            style={{
+              padding: "5%",
+              position: "absolute",
+              height: "2rem",
+              width: "3re",
+            }}
+          />
+        </Link>
+        <ul className="gronic-navUlist">
+          <MainNavLinks links={navLinksData} />
+        </ul>
+        <a onClick={toggleNav} className="gronic-burger">
+          <MdMenu />{" "}
+        </a>
+      </nav>
+      {showSmallNav && (
+        <nav className="gronic-smallNav">
+          <button onClick={toggleNav} className="gronic-burger">
+            <FaTimes />{" "}
+          </button>
+          <ul>
+            <SmallNavLinks showSmallNav={showSmallNav} links={navLinksData} />
+          </ul>
+        </nav>
+      )}
+    </>
+  )
+}
 
 export default Navbar
--- client/src/pages/solutions/solutions.en.js
+++ client/src/pages/solutions/solutions.en.js
@@ -58,70 +58,72 @@ const SolutionsPage = () => {
     <Layout>
       <SEO title="Gronic Solutions" />
       <div className="about-page">
-      <section className="gronic-solutionsSection section1">
-        <h1>{solutionsData.section1.title}</h1>
-        <p>{solutionsData.section1.description}</p>
-      </section>
-      <section className="gronic-solutionsSection section2">
-        <h1>{solutionsData.section2.title}</h1>
-        <div className="gronic-underline" />
-        <div className="solutionsTable">
-          <SolutionsTable data={solutionsData.section2.tableItems} />
-        </div>
-      </section>
-      <section className="gronic-solutionsSection section3">
-        <h1>{solutionsData.section3.title}</h1>
-        <div className="gronic-underline" />
-        <p> {solutionsData.section3.description1}</p>
-        <p> {solutionsData.section3.description2}</p>
-      </section>
-      <section id="attrasafe" className="gronic-solutionsSection section4">
-        <h1>{solutionsData.section4.title}</h1>
-        <div className="gronic-underline" />
-        <div className="subSections-container">
-          <div className="sol-subsection">
-            <h2>{solutionsData.section4.subSections[0].title}</h2>
-            <p>{solutionsData.section4.subSections[0].description} </p>
+        <section className="gronic-solutionsSection section1">
+          <h1>{solutionsData.section1.title}</h1>
+          <p>{solutionsData.section1.description}</p>
+        </section>
+        <section className="gronic-solutionsSection section2">
+          <h1>{solutionsData.section2.title}</h1>
+          <div className="gronic-underline" />
+          <div className="solutionsTable">
+            <SolutionsTable data={solutionsData.section2.tableItems} />
           </div>
+        </section>
+        <section className="gronic-solutionsSection section3">
+          <h1>{solutionsData.section3.title}</h1>
+          <div className="gronic-underline" />
+          <p> {solutionsData.section3.description1}</p>
+          <p> {solutionsData.section3.description2}</p>
+        </section>
+        <section id="attrasafe" className="gronic-solutionsSection section4">
+          <h1>{solutionsData.section4.title}</h1>
+          <div className="gronic-underline" />
+          <div className="subSections-container">
+            <div className="sol-subsection">
+              <h2>{solutionsData.section4.subSections[0].title}</h2>
+              <p>{solutionsData.section4.subSections[0].description} </p>
+            </div>
 
-          <div className="sol-subsection">
-            <h2>{solutionsData.section4.subSections[1].title}</h2>
-            <p>{solutionsData.section4.subSections[1].description} </p>
+            <div className="sol-subsection">
+              <h2>{solutionsData.section4.subSections[1].title}</h2>
+              <p>{solutionsData.section4.subSections[1].description} </p>
+            </div>
+            <div className="sol-subsection">
+              <h2>{solutionsData.section4.subSections[2].title}</h2>
+              <p>{solutionsData.section4.subSections[2].description} </p>
+            </div>
           </div>
-          <div className="sol-subsection">
-            <h2>{solutionsData.section4.subSections[2].title}</h2>
-            <p>{solutionsData.section4.subSections[2].description} </p>
-          </div>
-        </div>
-      </section>
-      <section id="biosafe" className="gronic-solutionsSection section5">
-        <h1>{solutionsData.section5.title}</h1>
-        <div className="gronic-underline" />
-        <div className="subSections-container">
-          <div className="sol-subsection">
-            <h2>{solutionsData.section4.subSections[0].title}</h2>
-            <p>{solutionsData.section5.subSections[0].description} </p>
-          </div>
+        </section>
+        <section id="biosafe" className="gronic-solutionsSection section5">
+          <h1>{solutionsData.section5.title}</h1>
+          <div className="gronic-underline" />
+          <div className="subSections-container">
+            <div className="sol-subsection">
+              <h2>{solutionsData.section4.subSections[0].title}</h2>
+              <p>{solutionsData.section5.subSections[0].description} </p>
+            </div>
 
-          <div className="sol-subsection">
-            <h2>{solutionsData.section5.subSections[1].title}</h2>
-            <p>{solutionsData.section5.subSections[1].description} </p>
+            <div className="sol-subsection">
+              <h2>{solutionsData.section5.subSections[1].title}</h2>
+              <p>{solutionsData.section5.subSections[1].description} </p>
+            </div>
+            <div className="sol-subsection">
+              <h2>{solutionsData.section4.subSections[2].title}</h2>
+              <p>{solutionsData.section4.subSections[2].description} </p>
+            </div>
           </div>
-          <div className="sol-subsection">
-            <h2>{solutionsData.section4.subSections[2].title}</h2>
-            <p>{solutionsData.section4.subSections[2].description} </p>
-          </div>
-        </div>
-      </section>
-      <section id="ecosafe" className="gronic-solutionsSection section6">
+        </section>
+        <section id="ecosafe" className="gronic-solutionsSection section6">
           <h1>{solutionsData.section6.title} </h1>
-          <div className="gronic-underline"/>
+          <div className="gronic-underline" />
           <div className="section-6-container">
-              <p>{solutionsData.section6.description}</p>
-              <a className="gronic-btn" href="/en/ecosafe">Learn more </a>
+            <p>{solutionsData.section6.description}</p>
+            <a className="gronic-btn" href="/en/ecosafe">
+              Learn more{" "}
+            </a>
           </div>
-      </section>
-  </div>
+        </section>
+      </div>
     </Layout>
   )
 }
