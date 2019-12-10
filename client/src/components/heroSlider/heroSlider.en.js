3e816f629f450cc2666601b8d62d0af17bf1bd95
Amr Abo Elenin <amr_aboelenin95@yahoo.com>
2019-12-10 17:37:48 +0200

    change english links to arabic

--- client/src/components/heroSlider/heroSlider.en.js
+++ client/src/components/heroSlider/heroSlider.en.js
@@ -95,7 +95,7 @@ const HeroSlider = () => {
   return (
     <>
       <div className='gronic-heroSlider'>
-        <SliderItems items={data.allHomePageJson.nodes[1].heroSlider} />
+        <SliderItems items={data.allHomePageJson.nodes[0].heroSlider} />
       </div>
     </>
   );
